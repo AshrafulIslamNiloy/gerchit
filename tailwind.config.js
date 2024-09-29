7df1750821924a7c9e341cf97f0a42691577cf29
AshrafulIslamNiloy <niloyahamed.cit.bd@gmail.com>
2024-09-29 22:36:57 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'Cormorant': ['Cormorant Upright', 'serif',],
+      'Open': ['Open Sans', 'sans-serif',],
+      
+      
+
+
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
